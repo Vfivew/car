dbc98e75b2f788dc3189a3a65a106f0837691b0d
Vfivew <hitori555111555@yahoo.com>
2024-05-12 20:07:48 +0300

    fix(frontend): change slider  tm-none

--- apps/frontend/src/libs/components/slider/slider.tsx
+++ apps/frontend/src/libs/components/slider/slider.tsx
@@ -1,12 +1,12 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import { A11y, Pagination, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import defaultAvatar from "~/assets/img/default-avatar.png";
 import { useInView } from "~/libs/hooks/hooks.js";
 
 import { Image } from "../image/image.js";
-import { settings } from "./libs/config/setting.config.js";
 import styles from "./styles.module.css";
 
 type Properties = {
@@ -25,26 +25,44 @@ const Carousel: React.FC<Properties> = ({ data }: Properties) => {
 			ref={ref}
 		>
 			<div className={styles["container-wrapper"]}>
-				<Slider {...settings}>
+				<Swiper
+					breakpoints={{
+						320: {
+							slidesPerView: 1,
+						},
+						480: {
+							slidesPerView: 2,
+						},
+						768: {
+							slidesPerView: 3,
+						},
+					}}
+					modules={[Pagination, Scrollbar, A11y]}
+					navigation
+					pagination={{ clickable: true }}
+					scrollbar={{ draggable: true }}
+					spaceBetween={40}
+				>
 					{data.map((item) => (
-						<div className={styles["slider"]} key={item.name}>
-							<div className={styles["image-wrapper"]}>
-								<Image
-									alt="avatar"
-									className={styles["image"]}
-									key={item.img ?? defaultAvatar}
-									shape="circle"
-									src={item.img ?? defaultAvatar}
-								/>
+						<SwiperSlide key={item.name}>
+							<div className={styles["slider"]}>
+								<div className={styles["image-wrapper"]}>
+									<Image
+										alt="avatar"
+										className={styles["image"]}
+										key={item.img ?? defaultAvatar}
+										shape="circle"
+										src={item.img ?? defaultAvatar}
+									/>
+								</div>
+								<div className={styles["content-wrapper"]}>
+									<p className={styles["title"]}>{item.name}</p>
+									<p className={styles["text"]}>{item.review}</p>
+								</div>
 							</div>
-
-							<div className={styles["content-wrapper"]}>
-								<p className={styles["title"]}>{item.name}</p>
-								<p className={styles["text"]}>{item.review}</p>
-							</div>
-						</div>
+						</SwiperSlide>
 					))}
-				</Slider>
+				</Swiper>
 			</div>
 		</div>
 	);
